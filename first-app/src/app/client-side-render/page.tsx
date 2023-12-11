8241bedbeb125d30dff766ea10fd674d7e6fa971
Cal <CalMail>
2023-12-11 17:47:45 +0000

    Add board folder structure and remove old components

--- first-app/src/app/client-side-render/page.tsx
+++ first-app/src/app/client-side-render/page.tsx
@@ -1,9 +1,9 @@
 'use client'
 
 import { useState, useEffect } from 'react';
-import BasicBoard from '../components/BasicBoard';
-import JsonBoard from '../components/JsonBoard';
-import NewBoard from '../components/NewBoardEntry';
+import BasicBoard from '../components/basic-board/BasicBoard';
+import JsonBoard from '../components/json-board/JsonBoard';
+import NewBoard from '../components/new-board/NewBoardEntry';
 
 enum board  {
   json,
---
+++ first-app/src/app/components/new-board/NewBoardEntry.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function NewBoardEntry({PlatformData}) {
+
+    return (
+        <div>
+            <table className="table table-sm">
+                <>
+            </table>
+            <label typeof="text"></label>
+        </div>
+    );
+}
